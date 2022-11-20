cfa92da3cbffbddcadc6ba60e4deb4ca2f844d44
Artem <artem.ysanov2217@gmail.com>
2022-11-20 15:10:52 +0300

    text

---
+++ src/main/java/com/example/practic3/Auto1Controller.java
@@ -0,0 +1,50 @@
+package com.example.practic3;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+public class Auto1Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ImageView auto1;
+
+    @FXML
+    private Button button1;
+
+    @FXML
+    void initialize() {
+        button1.setOnAction(event -> {
+            button1.getScene().getWindow().hide();
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("home.fxml"));
+
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+        });
+
+    }
+
+}
--- src/main/java/com/example/practic3/HomeController.java
+++ src/main/java/com/example/practic3/HomeController.java
@@ -1,10 +1,14 @@
 package com.example.practic3;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 public class HomeController {
 
@@ -27,12 +31,78 @@ public class HomeController {
     private ImageView auto4;
 
     @FXML
-    private Button favouritesHome;
-
-    @FXML
     void initialize() {
+        auto1.setOnMouseClicked(event -> {
+            auto1.getScene().getWindow().hide();
 
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("auto1.fxml"));
 
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        });
+
+        auto2.setOnMouseClicked(event -> {
+            auto2.getScene().getWindow().hide();
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("auto2.fxml"));
+
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        });
+
+        auto3.setOnMouseClicked(event -> {
+            auto3.getScene().getWindow().hide();
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("auto3.fxml"));
+
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        });
+
+        auto4.setOnMouseClicked(event -> {
+            auto4.getScene().getWindow().hide();
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("auto4.fxml"));
+
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        });
     }
 
 }
